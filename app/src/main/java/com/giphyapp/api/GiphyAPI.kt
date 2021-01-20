3728f6fd02b63905fde3c74f1da9bc513ec054f9
emujanovic3 <emujanovic3@etf.unsa.ba>
2021-01-20 19:41:15 +0100

    Retrofit classes created

---
+++ app/src/main/java/com/giphyapp/api/GiphyAPI.kt
@@ -0,0 +1,26 @@
+package com.giphyapp.api
+
+import com.giphyapp.BuildConfig
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GiphyAPI {
+    @GET("/v1/gifs/trending")
+    suspend fun getTrendingGifs(
+        @Query("offset")
+        offset: Int = 0,
+        @Query("api_key")
+        apiKey: String = BuildConfig.GiphySecAPIKey
+    ): Response<GiphyResponse>
+
+    @GET("v1/gifs/search")
+    suspend fun searchGifs(
+        @Query("q")
+        query: String,
+        @Query("offset")
+        offset: Int = 0,
+        @Query("api_key")
+        apiKey: String = BuildConfig.GiphySecAPIKey
+    ): Response<GiphyResponse>
+}
